0b2444d87697cd2198736b7cd1fb17d2a912e3fd
IonianCTF <c64kara@gmail.com>
2022-06-10 15:48:53 +0300

    Add files via upload

---
+++ ninjafirewall/nfwlog/firewall_2020-04.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+[1586714318] [0] [192.168.43.134] [#6957584] [0] [6] [192.168.43.127] [200 OK] [GET] [/ninjafirewall/index.php] [Log deleted by admin] [administrator: firewall_2020-04.php]
+[1586714870] [0.00145] [192.168.43.134] [#8996352] [1] [3] [192.168.43.121] [403] [GET] [/development/tools/FileInclusion/pages/fetchmeafile.php] [Directory traversal] [hex:4745543a66696c65203d202e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f6574632f7061737377642f6a756c6975736977696c6c6b696c6c796f75]
+[1586715012] [0.00028] [192.168.43.134] [#3756986] [0] [3] [192.168.43.121] [403] [POST] [/development/tools/FileUpload/fileupload.php] [Attempt to upload a script or system file] [hex:6966746869736765747375706c6f616465646977696c6c6b696c6c796f756a756c6975732e7068702028323020627974657329]
+[1586778008] [0.00459] [cryptobank.local] [#5426242] [1] [3] [192.168.1.101] [403] [GET] [/development/tools/FileInclusion/pages/fetchmeafile.php] [Directory traversal] [hex:4745543a66696c65203d202e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f2e2e2f7661722f6c6f672f617061636865322f6163636573732e6c6f67]
